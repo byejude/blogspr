1690c76ebc2864dc72fb968733eb0bf257f2efc5
tulip <edelweissasuare@163.com>
2017-12-25 07:28:02 +0800

    initialize project

---
+++ src/main/java/com/tulip/blogspri/BlogspriApplication.java
@@ -0,0 +1,12 @@
+package com.tulip.blogspri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogspriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogspriApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tulip/blogspri/BlogspriApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tulip.blogspri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogspriApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
